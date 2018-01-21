d1a0f5f5915e333349e8cab6d1a9ea165f4eb451
Conner Petzold <cpetzold@gmail.com>
2018-01-20 22:53:49 -0800

    Movement based rotation

---
+++ Assets/MovementRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRotation : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+		if (rb.velocity.magnitude > 0) {
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
+		}
+	}
+}
